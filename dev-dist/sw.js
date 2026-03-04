77b82133688b853aa5a6660206a9b8664fc389c1
george-karl-real <rgeorgekarl@gmail.com>
2026-03-04 16:42:10 +0800

    barcode scanner integration

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.54ij77b4qg8"
+    "revision": "0.4fv67lvc1c"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
